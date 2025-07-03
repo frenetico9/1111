e3dfc2e320fa8d1231edf9d0513d9bbfdf52fb09
frenetico9 <hiago182016@gmail.com>
2025-07-03 10:13:16 -0300

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,17 +1,14 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 
-interface LayoutProps {
-  children: ReactNode;
-}
-
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+const Layout: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen bg-white">
       <Header />
-      <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-6 sm:py-8">
-        {children}
+      <main className="flex-grow">
+        <Outlet />
       </main>
       <Footer />
     </div>
