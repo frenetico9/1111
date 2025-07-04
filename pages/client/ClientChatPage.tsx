4ee4a194deaa4b90624948620f93bd63f9297749
frenetico9 <hiago182016@gmail.com>
2025-07-04 12:22:28 -0300

    Update ClientChatPage.tsx

--- pages/client/ClientChatPage.tsx
+++ pages/client/ClientChatPage.tsx
@@ -13,9 +13,7 @@ import {
 } from '../../services/mockApiService';
 import LoadingSpinner from '../../components/LoadingSpinner';
 import { useNotification } from '../../contexts/NotificationContext';
-import { format } from 'date-fns/format';
-import { parseISO } from 'date-fns/parseISO';
-import { formatDistance } from 'date-fns/formatDistance';
+import { format, parseISO, formatDistance } from 'date-fns';
 import { ptBR } from 'date-fns/locale/pt-BR';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
@@ -158,7 +156,7 @@ const ClientChatPage: React.FC = () => {
           await refreshUnreadCount();
         }
       } catch (error) {
-        addNotification({ message: "Falha ao carregar a conversa.", type: "error" });
+        addNotification({ message: (error as Error).message || "Falha ao carregar a conversa.", type: "error" });
         navigate('/client/chat', { replace: true });
       } finally {
         setLoadingMessages(false);
